fedfa05ffbeca7fc112aeb8decb574e4edd473fc
PetarOtovic <otovicpetar1998@gmail.com>
2023-07-16 00:17:35 +0200

    Minor bug fixes and changes

--- lib/bloc/bloc_base.dart
+++ lib/bloc/bloc_base.dart
@@ -4,6 +4,7 @@ abstract class Bloc<T> {
   final StreamController<T> _controller = StreamController<T>.broadcast();
   StreamSink<T> get streamSink => _controller.sink;
   Stream<T> get stream => _controller.stream;
+  Function? _condition;
 
   late T state;
 
@@ -11,8 +12,16 @@ abstract class Bloc<T> {
     streamSink.add(this.state);
   }
 
+  void setCondition(Function condition) {
+    this._condition = condition;
+  }
+
   void add(T value) async {
-    streamSink.add(value);
+    if (this._condition != null) {
+      if (this._condition == true) streamSink.add(value);
+    } else {
+      streamSink.add(value);
+    }
     state = value;
   }
 }
--- lib/reactive/reactive_widget.dart
+++ lib/reactive/reactive_widget.dart
@@ -1,45 +1,24 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:reactive/bloc/bloc_base.dart';
 
-class ReactiveWidget<S> extends StatefulWidget {
-  final Bloc<S> bloc;
+class ReactiveWidget<T> extends StatelessWidget {
+  final Bloc<T> bloc;
   final Function builder;
+  final Function? condition;
 
-  const ReactiveWidget({
-    Key? key,
-    required this.bloc,
-    required this.builder,
-  }) : super(key: key);
-
-  @override
-  _ReactiveWidgetState<S> createState() => _ReactiveWidgetState<S>();
-}
-
-class _ReactiveWidgetState<S> extends State<ReactiveWidget<S>> {
-  StreamSubscription<S>? _subscription;
-  S? _snapshotData;
-
-  @override
-  void initState() {
-    super.initState();
-    _snapshotData = widget.bloc.state;
-  }
-
-  @override
-  void dispose() {
-    _subscription?.cancel();
-    super.dispose();
+  ReactiveWidget(
+      {Key? key, this.condition, required this.bloc, required this.builder})
+      : super(key: key) {
+    if (this.condition != null) this.bloc.setCondition(this.condition!);
   }
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: widget.bloc.stream,
-      initialData: widget.bloc.state,
-      builder: (BuildContext context, AsyncSnapshot<S> snap) =>
-          widget.builder(context, snap.data),
+      stream: bloc.stream,
+      initialData: bloc.state,
+      builder: (BuildContext context, AsyncSnapshot<T> snap) =>
+          builder(context, snap.data),
     );
   }
 }
